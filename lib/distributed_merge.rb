175c2d875112e69fa9242fd6ab4e18c52383f9b1
Suleyman Musayev <slmusayev@gmail.com>
2023-04-09 15:14:25 +0500

    add initial commit

---
+++ lib/distributed_merge.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "distributed_merge/version"
+
+module DistributedMerge
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "distributed_merge"
+
+require "minitest/autorun"
